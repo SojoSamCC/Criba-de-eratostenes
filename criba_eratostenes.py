0e04e18f80c509f7208ea0452429d7338eaa0c7f
Samuel <academicsamuel02@gmail.com>
2024-03-28 11:43:20 -0300

    Implementación para uso de un usuario

--- criba_eratostenes.py
+++ criba_eratostenes.py
@@ -1,6 +1,31 @@
 import math
+num_giones=100
+print()
+print("¡Hola!")
+print()
+print("Te doy la bienvenida a este pequeño programita para hallar todos los números primos")
+print("que se encuentren antes del número que tú me digas (mejor conocido como criba de eratóstenes).")
+print()
+print("Si deseas apoyarme te dejo mi alias de Mercado Pago Argentina: sojo.sam.mp")
+print()
+print("También te dejo mi ig por si deseas seguirme: @therealdusa")
+print()
+print("Y por último (pero no por eso menos importante) te dejo mi perfil de GitHub")
+print("para que lo chusmees (me gusta programar cosas relacionadas a las materias de la facultad o ")
+print("que sean de mi interés):")
+print()
+print("https://github.com/SojoSamCC")
+print()
+print("Bueeeeno, ahora sí. Comencemosss")
+print()
 
-def criba_eratostenes(n):
+for _ in range(num_giones):
+    print("-", end="")
+print()
+def criba_eratostenes():
+    print()
+    print("Ingresa el número del cual quieres saber los números primos", end=" ")
+    n=int(input("que le preceden: "))
     raiz_cuadrada_de_n_redondeada_para_abajo=int(math.floor(math.sqrt(n)))
     lista_numeros_primos=[]
     lista_numeros_no_primos=[]
@@ -14,6 +39,29 @@ def criba_eratostenes(n):
                         lista_numeros_no_primos.append(k*j)
     for l in range (len(lista_numeros_no_primos)):
         lista_numeros_primos.remove(lista_numeros_no_primos[l])
-    return lista_numeros_primos
+    print()
+    return print(f"Todos los números primos que encuentran antes del dado son: {lista_numeros_primos}")
+criba_eratostenes()
 
-print(criba_eratostenes(1))
+while True:
+    print()
+    repetir_criba=input("¿Deseas repetir la criba de eratóstenes? (Y/n): ")
+    if repetir_criba!="n":
+        criba_eratostenes()
+    else:
+        print()
+        break
+
+for _ in range(num_giones):
+    print("-", end="")
+
+print()
+print()
+print("¡Muchas gracias por haccer uso de este programita! Espero te haya sido de ayuda.")
+print()
+print("Te deseo un muy buen día.")
+print()
+print("-SojoSam")
+print()
+input("--Presiona cualquier tecla para cerrar el programa--")
+exit()
